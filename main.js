85208a7f108fda484567adfe551c842a7d3f8d58
chamd <50565709+chamd@users.noreply.github.com>
2022-03-17 19:40:58 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function getel(e) {
     return document.getElementById(e)
 }
 
-function go() {
+function setSeat() {
     var list = document.getElementsByClassName('seat');
 
     for (let i = 0; i < list.length; i++) {
@@ -57,12 +57,28 @@ function go() {
     }, 1000);
 }
 
+var test = [];
+
+for (let i = 1; i <= 25; i++) {
+    test.push(`조찬아${i}`);
+}
+
 var ids = 1
 
 for (let i = 1; i <= 5; i++) {
     for (let j = 1; j <= 5; j++) {
-        document.getElementById('content').innerHTML += `<div class='seat a${i} b${j}' id='${ids}'>${ids}</div>`;
+        document.getElementById('content').innerHTML += `<div class='seat a${i} b${j}' id='${ids}'>${test[ids-1]}</div>`;
         ids++;
     }
 }
 
+document.body.addEventListener("keydown", function(e){
+    if(e.keyCode == 13){
+        getel('nameInput').value = '';
+    }
+});
+
+function setName() {
+    getel('nameInput').classList.toggle('inv');
+    getel('nameInput').select();
+}
