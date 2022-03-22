913747454d0bc961bc665c047b514b46a4acb3ba
chamd <50565709+chamd@users.noreply.github.com>
2022-03-22 19:10:51 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -7,20 +7,7 @@ var selectedSeat;
 var pinnedSeat = [];
 
 function changeSeat(n) {
-    if (nowMode == 1) {
-        if (getel(n).classList[2] != 'pin') {
-            getel(n).style.backgroundColor = 'rgba(255, 95, 95, 0.5)';
-            pinnedSeat.push(getel(n).className);
-            getel(n).classList.add('pin');
-        } else {
-            getel(n).style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
-            getel(n).classList.remove('pin');
-            var _index = pinnedSeat.indexOf(getel(n).className);
-            if (_index != -1) {
-                pinnedSeat.splice(_index, 1);
-            }
-        }
-    } else {
+    if (nowMode == 0) {
         if (!isSelectSeat) {
             getel(n).style.backgroundColor = 'rgba(69, 52, 218, 0.5)';
             selectedSeat = n;
@@ -38,7 +25,25 @@ function changeSeat(n) {
             }, 300);
             selectedSeat = '';
             isSelectSeat = false;
-        }    
+        }
+    } if (nowMode == 1) {
+        if (getel(n).classList[2] != 'pin') {
+            getel(n).style.backgroundColor = 'rgba(255, 95, 95, 0.5)';
+            pinnedSeat.push(getel(n).className);
+            getel(n).classList.add('pin');
+        } else {
+            getel(n).style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+            getel(n).classList.remove('pin');
+            var _index = pinnedSeat.indexOf(getel(n).className);
+            if (_index != -1) {
+                pinnedSeat.splice(_index, 1);
+            }
+        }
+    } else if (nowMode == 2) {
+        if (seats[n - 1] != undefined) {
+            seats.splice(n - 1, 1);
+            formatSeat();
+        }
     }
 }
 
@@ -47,10 +52,12 @@ function setHover() {
     Array.from(els).forEach(function (el) {
         el.addEventListener('mouseover', (e) => {
             el.style.transition = '0.1s';
-            if (nowMode == 1) {
+            if (nowMode == 0) {
+                el.style.boxShadow = 'rgb(69, 52, 218) 0px 0px 0px 5px';
+            } else if (nowMode == 1) {
                 el.style.boxShadow = 'rgb(255, 95, 95) 0px 0px 0px 5px';
             } else {
-                el.style.boxShadow = 'rgb(69, 52, 218) 0px 0px 0px 5px';
+                el.style.boxShadow = 'rgb(255, 251, 37) 0px 0px 0px 5px';
             }
         });
         el.addEventListener('mouseout', (e) => {
@@ -70,6 +77,7 @@ function formatSeat() {
 
     for (let i = 1; i <= a; i++) {
         for (let j = 1; j <= b; j++) {
+            detectBlank(ids - 1, 0);
             if (pinnedSeat.indexOf(`a${i} b${j}`) == -1) {
                 if (seats[ids - 1]) {
                     getel('content').innerHTML += `<div seat='true' class='a${i} b${j}' id='${ids}' onclick='changeSeat(${ids});'>${seats[ids - 1]}</div>`;
@@ -93,32 +101,28 @@ function formatSeat() {
     setHover();
 }
 
-var isSaveload = true;
-
 function formatVars(_a, _b) {
     a = _a;
     b = _b;
     all = a * b;
-    
+
     document.documentElement.style.setProperty('--num3', b);
     document.documentElement.style.setProperty('--num4', a);
-
-    if (b == 4) {
-        isSaveload = false;
-        document.getElementsByClassName('save')[0].style.display = 'none';
-        document.getElementsByClassName('load')[0].style.display = 'none';
-        document.getElementsByClassName('save')[0].style.marginLeft = '135px';
-        document.getElementsByClassName('load')[0].style.marginLeft = '225px';
-        document.getElementsByClassName('wid')[0].style.display = 'none';
-        document.getElementsByClassName('hei')[0].style.display = 'none';
-        document.getElementsByClassName('wid')[0].style.marginRight = '225px';
-        document.getElementsByClassName('hei')[0].style.marginRight = '135px';
-    }
 
     formatSeat();
 }
 
-formatVars(6, 4);
+formatVars(5, 5);
+
+function detectBlank(n, _b) {
+    if (seats[n] == '') {
+        if (n == all - 1) {
+            seats.splice(n - _b, _b + 1);
+        } else {
+            detectBlank(n + 1, _b + 1);
+        }
+    }
+}
 
 function getel(e) {
     return document.getElementById(e)
@@ -133,7 +137,7 @@ function openSLMenu() {
         document.getElementsByClassName('load')[0].style.display = 'block';
         getel('slMenuCover').style.display = 'block';
 
-        isSLMenuOpen = true;    
+        isSLMenuOpen = true;
         if (isWHMenuOpen) {
             openWHMenu();
         }
@@ -143,7 +147,7 @@ function openSLMenu() {
         document.getElementsByClassName('load')[0].style.display = 'none';
         getel('slMenuCover').style.display = 'none';
 
-        isSLMenuOpen = false;    
+        isSLMenuOpen = false;
     }
 }
 
@@ -156,7 +160,7 @@ function openWHMenu() {
         document.getElementsByClassName('hei')[0].style.display = 'block';
         getel('whMenuCover').style.display = 'block';
 
-        isWHMenuOpen = true;    
+        isWHMenuOpen = true;
         if (isSLMenuOpen) {
             openSLMenu();
         }
@@ -166,58 +170,66 @@ function openWHMenu() {
         document.getElementsByClassName('hei')[0].style.display = 'none';
         getel('whMenuCover').style.display = 'none';
 
-        isWHMenuOpen = false;    
+        isWHMenuOpen = false;
     }
 }
 
 function setSeat() {
-    var list = document.querySelectorAll('div[seat]');
+    if (document.getElementsByClassName('btn1')[0].className != 'btn1 cant') {
+        document.getElementsByClassName('btn1')[0].classList.add('cant');
+        document.getElementsByClassName('btn1')[0].style.boxShadow = 'rgb(255, 95, 95) 0px 0px 0px 5px';
 
-    for (let i = 0; i < list.length; i++) {
-        var e = list[i];
-        if (e.classList[2] != 'pin') {
-            e.className = '';
-            e.classList.add('dispenser');    
-        }
-    }
+        var list = document.querySelectorAll('div[seat]');
 
-    setTimeout(() => {
-        var _seats = [];
-
-        for (let i = 1; i <= a; i++) {
-            for (let j = 1; j <= b; j++) {
-                if (!document.getElementsByClassName(`a${i} b${j} pin`)[0]) {
-                    _seats.push(['a' + i, 'b' + j]);
-                }
+        for (let i = 0; i < list.length; i++) {
+            var e = list[i];
+            if (e.classList[2] != 'pin') {
+                e.className = '';
+                e.classList.add('dispenser');
             }
         }
-
-        for (let i = 0; i < all; i++) {
-            (function (x) {
-                setTimeout(function () {
-                    var r = Math.floor(Math.random() * _seats.length);
-                    if (getel(i + 1).classList[2] != 'pin') {
-                        var seat = _seats[r];
-                        getel(i + 1).classList.remove('dispenser');
-                        getel(i + 1).classList.add(seat[0], seat[1]);  
-                        _seats.splice(r, 1);  
+    
+        setTimeout(() => {
+            var _seats = [];
+    
+            for (let i = 1; i <= a; i++) {
+                for (let j = 1; j <= b; j++) {
+                    if (!document.getElementsByClassName(`a${i} b${j} pin`)[0]) {
+                        _seats.push(['a' + i, 'b' + j]);
                     }
-                    if (i == all - 1) {
-                        setTimeout(() => {
-                            seats = [];
-                            for (let i = 1; i <= a; i++) {
-                                for (let j = 1; j <= b; j++) {
-                                    seats.push(document.getElementsByClassName(`a${i} b${j}`)[0].innerHTML);
+                }
+            }
+    
+            for (let i = 0; i < all; i++) {
+                (function (x) {
+                    setTimeout(function () {
+                        var r = Math.floor(Math.random() * _seats.length);
+                        if (getel(i + 1).classList[2] != 'pin') {
+                            var seat = _seats[r];
+                            getel(i + 1).classList.remove('dispenser');
+                            getel(i + 1).classList.add(seat[0], seat[1]);
+                            _seats.splice(r, 1);
+                        }
+                        if (i == all - 1) {
+                            setTimeout(() => {
+                                seats = [];
+                                for (let i = 1; i <= a; i++) {
+                                    for (let j = 1; j <= b; j++) {
+                                        seats.push(document.getElementsByClassName(`a${i} b${j}`)[0].innerHTML);
+                                    }
                                 }
-                            }
-                            formatSeat();
-                        }, 1000);
-                    }
-                }, 150 * x);
-            })(i);
-        }
-
-    }, 1000);
+                                formatSeat();
+                                document.getElementsByClassName('btn1')[0].classList.remove('cant');
+                                document.getElementsByClassName('btn1')[0].style.boxShadow = 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px';
+                            }, 1000);
+                        }
+                    }, 150 * x);
+                })(i);
+            }
+    
+        }, 1000);
+    
+    }
 }
 
 getel('nameInput').addEventListener("keydown", function (e) {
@@ -239,16 +251,8 @@ function option() {
         getel('nameOuter').style.height = `${a * 110 - 50}px`;
         getel('nameList').style.height = `${a * 110 - 80}px`;
         getel('nameInput').style.display = 'inline';
-        if (isSaveload) {
-            document.getElementsByClassName('save')[0].style.display = 'block';
-            document.getElementsByClassName('load')[0].style.display = 'block';
-            document.getElementsByClassName('wid')[0].style.display = 'block';
-            document.getElementsByClassName('hei')[0].style.display = 'block';   
-        } else {
-            document.getElementsByClassName('saveload')[0].style.display = 'block';    
-            document.getElementsByClassName('widhei')[0].style.display = 'block';    
-        }
-         
+        document.getElementsByClassName('saveload')[0].style.display = 'block';
+        document.getElementsByClassName('widhei')[0].style.display = 'block';
 
         isPopup = true;
     } else {
@@ -263,10 +267,10 @@ function option() {
             document.getElementsByClassName('saveload')[0].style.display = 'none';
             document.getElementsByClassName('saveload')[0].style.boxShadow = 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px';
             document.getElementsByClassName('wid')[0].style.display = 'none';
-            document.getElementsByClassName('hei')[0].style.display = 'none';    
+            document.getElementsByClassName('hei')[0].style.display = 'none';
             document.getElementsByClassName('widhei')[0].style.display = 'none';
             document.getElementsByClassName('widhei')[0].style.boxShadow = 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px';
-    
+
             isSLMenuOpen = false;
             getel('slMenuCover').style.display = 'none';
             isWHMenuOpen = false;
@@ -312,7 +316,8 @@ function openTextFile() {
 }
 
 function removeSeat(n) {
-    seats.splice(n, 1);
+    // seats.splice(n, 1);
+    seats[n] = '';
     formatSeat();
 }
 
@@ -321,24 +326,24 @@ var nowMode = 0;
 function changeMode() {
     if (nowMode == 0) {
         document.documentElement.style.setProperty('--mode', '\'모드: 자리고정\'');
-        document.getElementsByClassName('mode')[0].innerHTML = '<i class="fa-solid fa-thumbtack"></i>';    
+        document.getElementsByClassName('mode')[0].innerHTML = '<i class="fa-solid fa-thumbtack"></i>';
         nowMode = 1;
 
-        getel(selectedSeat).style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+        // getel(selectedSeat).style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
         formatSeat();
         selectedSeat = '';
         isSelectSeat = false;
 
     } else if (nowMode == 1) {
-        // document.documentElement.style.setProperty('--mode', '\'모드: 이름변경\'');
-        // document.getElementsByClassName('mode')[0].innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';    
-        // nowMode = 2;
-        document.documentElement.style.setProperty('--mode', '\'모드: 자리변경\'');
-        document.getElementsByClassName('mode')[0].innerHTML = '<i class="fa-solid fa-arrows-rotate"></i>';    
-        nowMode = 0;
+        document.documentElement.style.setProperty('--mode', '\'모드: 자리삭제\'');
+        document.getElementsByClassName('mode')[0].innerHTML = '<i class="fa-solid fa-trash-can"></i>';    
+        nowMode = 2;
+        // document.documentElement.style.setProperty('--mode', '\'모드: 자리변경\'');
+        // document.getElementsByClassName('mode')[0].innerHTML = '<i class="fa-solid fa-arrows-rotate"></i>';
+        // nowMode = 0;
     } else {
         document.documentElement.style.setProperty('--mode', '\'모드: 자리변경\'');
-        document.getElementsByClassName('mode')[0].innerHTML = '<i class="fa-solid fa-arrows-rotate"></i>';    
+        document.getElementsByClassName('mode')[0].innerHTML = '<i class="fa-solid fa-arrows-rotate"></i>';
         nowMode = 0;
     }
     setHover();
@@ -348,17 +353,24 @@ var nowWid = 5;
 var nowHei = 5;
 
 function changeWid() {
-    nowWid ++;
+    nowWid++;
     if (nowWid == 6) {
         nowWid = 4;
     }
     formatVars(nowHei, nowWid);
+
+    document.getElementsByClassName('wid')[0].innerHTML = `<i class="fa-solid fa-${nowWid}"></i>`;
 }
 
 function changeHei() {
-    nowHei ++;
+    nowHei++;
     if (nowHei == 7) {
         nowHei = 4;
     }
     formatVars(nowHei, nowWid);
+
+    document.getElementsByClassName('hei')[0].innerHTML = `<i class="fa-solid fa-${nowHei}"></i>`;
+    getel('popup').style.height = `${a * 110 + 130}px`;
+    getel('nameOuter').style.height = `${a * 110 - 50}px`;
+    getel('nameList').style.height = `${a * 110 - 80}px`;
 }
